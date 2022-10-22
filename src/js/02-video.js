bb89ff819b14dfef110f08e90348a2a1e4d46036
Fredy228 <smagrovich58@gmail.com>
2022-10-22 14:57:22 +0300

    Task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Vimeo from "@vimeo/player";
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const playerCurrentTime = Number(localStorage.getItem("videoplayer-current-time")) ;
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    player.on('timeupdate', throttle(setTimeLocalstorage, 1000));
+
+    function setTimeLocalstorage({seconds}) {
+        localStorage.setItem("videoplayer-current-time", seconds);
+    };
+
+    player.setCurrentTime(playerCurrentTime).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });

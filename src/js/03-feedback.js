ea1c00152680695bacc51d99af4f049531f19eac
Fredy228 <smagrovich58@gmail.com>
2022-10-22 16:31:45 +0300

    Task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+const throttle = require('lodash.throttle');
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector("input"),
+    textarea: document.querySelector("textarea"),
+};
+const formStorage = {};
+const STORAGE_KEY = "feedback-form-state";
+const getFormStorage = localStorage.getItem(STORAGE_KEY);
+
+refs.form.addEventListener("input", throttle(setValueStorage, 500));
+refs.form.addEventListener("submit", throttle(submitForm, 500))
+
+function setValueStorage(event) {
+    formStorage[event.target.name] = event.target.value;
+
+    const valueStorageJSON = JSON.stringify(formStorage);
+    localStorage.setItem(STORAGE_KEY, valueStorageJSON);
+};
+
+function submitForm(event) {
+    event.preventDefault();
+
+    console.log(`email: ${formStorage.email},
+    message: ${formStorage.message}`);
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+
+if (getFormStorage) {
+
+    const getValueStorage = JSON.parse(getFormStorage);
+
+    refs.input.value = getValueStorage.email;
+    refs.textarea.value = getValueStorage.message;
+
+    formStorage.email = getValueStorage.email;
+    formStorage.message = getValueStorage.message;
+}
 
